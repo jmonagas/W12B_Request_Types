a8102dfa269b84ea222062de0f474b41d04f0fc1
Jose <jmonagas@hotmail.ca>
2020-09-12 19:34:05 -0600

    Completed the assignment, tested all functions and everything is working well

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////////////////////////////////////
 
 function sendTweet() {
    let tweetTitle = document.getElementById("title-input").value;
@@ -11,13 +12,13 @@ function sendTweet() {
    ajax.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 201) {
          console.log(JSON.parse(this.responseText));
-         document.getElementById("target_status").innerHTML = "Success";
+         document.getElementById("target_status").innerHTML = "Success!";
       }
       else if (this.readyState != 4) {
-         document.getElementById("target_status").innerHTML = "Loading";
+         document.getElementById("target_status").innerHTML = "Loading!";
       }
       else {
-         document.getElementById("target_status").innerHTML = "Error";
+         document.getElementById("target_status").innerHTML = "Error!";
       }
    }
    ajax.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
@@ -27,65 +28,38 @@ function sendTweet() {
 let tweetButton = document.getElementById("tweet-submit");
 tweetButton.addEventListener("click", sendTweet);
 
+////////////////////////////////////////////////////////////////////////////////
 
 fetch("https://jsonplaceholder.typicode.com/posts/1", {
    method: "PATCH", body: JSON.stringify({ completed: true }),
    headers: { "Content-Type": "application/json; charset = UTF - 8" }
 }).then(response => response.json()).then(json => console.log(json))
 
+////////////////////////////////////////////////////////////////////////////////
 
 fetch("https://jsonplaceholder.typicode.com/posts/1", {
    method: "DELETE",
 }).then(response => response.json()).then(json => console.log(json))
 
+////////////////////////////////////////////////////////////////////////////////
 
 const logBtn = document.getElementById("btn");
 logBtn.addEventListener('click', fetchData);
 
 async function fetchData() {
-
    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
    const data = await response.json();
-
+   console.log(data);
    data.forEach(obj => {
-      Object.entries(obj).forEach(([title, body]) => {
-         console.log(`${title} ${body}`);
-
-         document.getElementById("tweet-logs").innerHTML = JSON.stringify(data);
-      });
-      console.log("==========================");
+      let tweet = document.createElement("div");
+      let tweet_title = document.createElement("h4");
+      let tweet_body = document.createElement("p");
+      tweet_title.innerText = "Subject: " + obj.title;
+      tweet_body.innerText = "Content: " + obj.body;
+      tweet.appendChild(tweet_title);
+      tweet.appendChild(tweet_body);
+      document.getElementById("tweet-logs").appendChild(tweet);
    });
 }
 
-
-
-// class tweetsLog {
-//    content;
-
-//    getNewTweets(html_target) {
-//       let myTweets = this;
-//       let ajax = new XMLHttpRequest();
-//       ajax.onreadystatechange = function () {
-
-//          if (this.readyState == 4 && this.status == 200) {
-//             myTweets.content = JSON.parse(this.responseText);
-//             console.log(this.responseText);
-
-//             let tweets_element = document.getElementById(html_target);
-//             tweets_element.innerText = myTweets.content;
-//          }
-//          else if (this.readyState != 4) {
-//             document.getElementById(html_target).innerHTML = "Processing";
-//          }
-//          else {
-//             document.getElementById(html_target).innerHTML = "Something Went Wrong";
-//          }
-//       }
-//       ajax.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
-//       ajax.send();
-//    }
-// }
-// document.getElementById("btn").addEventListener("click", function () {
-//    let myTweets = new tweetsLog();
-//    myTweets.getNewTweets("tweet-logs");
-// });
+////////////////////////////////////////////////////////////////////////////////
